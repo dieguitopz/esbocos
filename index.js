0181db947aa62ea36444def5266125d60042390d
Diego Pedroza <72617665+dieguitopz@users.noreply.github.com>
2023-01-15 16:18:52 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+let userDiego = {
+    altura: ': '+ 1.80,
+    idade: ': '+ 21,
+    status: ': Solteiro',
+    statusEnsino : ': Graduando' 
+
+
+}
+
+
+for (let i in userDiego){
+
+    console.log(i , userDiego[i])
+
+}
